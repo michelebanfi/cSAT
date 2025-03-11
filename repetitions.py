6769c9e97b6d3de6cf51577193fa5a5e55435b0e
michelebanfi <michi.banfi01@gmail.com>
2025-03-11 19:16:45 +0100

    yooo i didn't know about semilogy

--- repetitions.py
+++ repetitions.py
@@ -11,13 +11,26 @@ y_rounded = [math.ceil(n) for n in y]
 
 y_classical = [2**n for n in range(0, ranger - 5)]
 
-plt.scatter(x = x, y = y, label="Floating point iterations")
-plt.scatter(x = x, y = y_rounded, label="Integer iterations")
-plt.scatter(x = x[:-5], y = y_classical, label="Classical iterations")
+# Original linear plot
+plt.figure(figsize=(10, 6))
+plt.scatter(x=x, y=y, label="Floating point iterations")
+plt.scatter(x=x, y=y_rounded, label="Integer iterations")
+plt.scatter(x=x[:-5], y=y_classical, label="Classical iterations")
 plt.xlabel("Number of variables")
 plt.ylabel("Iterations")
 plt.legend()
 plt.yticks(y_rounded)
 plt.grid(True)
 plt.savefig("miscellanous/repetitions.png")
+
+# Logarithmic plot
+plt.figure(figsize=(10, 6))
+plt.semilogy(x, y, 'o', label="Floating point iterations")
+plt.semilogy(x, y_rounded, 'o', label="Integer iterations")
+plt.semilogy(x[:-5], y_classical, 'o', label="Classical iterations")
+plt.xlabel("Number of variables")
+plt.ylabel("Iterations (log scale)")
+plt.legend()
+plt.grid(True, which="both", ls="-")
+plt.savefig("miscellanous/repetitions_log.png")
 
