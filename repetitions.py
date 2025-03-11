8a5aa3ebe0c7c23c81e96f122f0faf7d6e8982d8
michelebanfi <michi.banfi01@gmail.com>
2025-03-11 19:13:51 +0100

    some thoughts about iterations

--- repetitions.py
+++ repetitions.py
@@ -2,17 +2,22 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 
-# x = np.arange(1, 30, step=1)
-# np.pi/4 * math.sqrt(2**n_variables)
-x = np.arange(10).tolist()
-y = [np.pi/4 * math.sqrt(2**n) for n in range(0, 10)]
+ranger = 10
+
+x = np.arange(ranger).tolist()
+y = [np.pi/4 * math.sqrt(2**n) for n in range(0, ranger)]
 
 y_rounded = [math.ceil(n) for n in y]
 
-plt.scatter(x = x, y = y, label="Floating point")
-plt.scatter(x = x, y = y_rounded, label="Integers")
+y_classical = [2**n for n in range(0, ranger - 5)]
+
+plt.scatter(x = x, y = y, label="Floating point iterations")
+plt.scatter(x = x, y = y_rounded, label="Integer iterations")
+plt.scatter(x = x[:-5], y = y_classical, label="Classical iterations")
+plt.xlabel("Number of variables")
+plt.ylabel("Iterations")
 plt.legend()
 plt.yticks(y_rounded)
 plt.grid(True)
-plt.show()
+plt.savefig("miscellanous/repetitions.png")
 
